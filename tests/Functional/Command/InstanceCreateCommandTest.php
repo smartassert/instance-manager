a2c7c941b99af2cdeb102fc18b7ce1d2344c287a
Jon Cram <webignition@gmail.com>
2021-10-11 12:38:58 +0100

    Import app:instance:create command (#22)

---
+++ tests/Functional/Command/InstanceCreateCommandTest.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Tests\Functional\Command;
+
+use App\Command\InstanceCreateCommand;
+use App\Tests\Services\HttpResponseFactory;
+use DigitalOceanV2\Exception\RuntimeException;
+use GuzzleHttp\Handler\MockHandler;
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class InstanceCreateCommandTest extends KernelTestCase
+{
+    private InstanceCreateCommand $command;
+    private HttpResponseFactory $httpResponseFactory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $command = self::getContainer()->get(InstanceCreateCommand::class);
+        \assert($command instanceof InstanceCreateCommand);
+        $this->command = $command;
+
+        $httpResponseFactory = self::getContainer()->get(HttpResponseFactory::class);
+        \assert($httpResponseFactory instanceof HttpResponseFactory);
+        $this->httpResponseFactory = $httpResponseFactory;
+    }
+
+    /**
+     * @dataProvider executeThrowsExceptionDataProvider
+     *
+     * @param array<mixed>             $responseData
+     * @param class-string<\Throwable> $expectedExceptionClass
+     */
+    public function testExecuteThrowsException(
+        array $responseData,
+        string $expectedExceptionClass,
+        string $expectedExceptionMessage,
+        int $expectedExceptionCode
+    ): void {
+        $httpResponse = $this->httpResponseFactory->createFromArray($responseData);
+        $this->setHttpResponse($httpResponse);
+
+        self::expectException($expectedExceptionClass);
+        self::expectExceptionMessage($expectedExceptionMessage);
+        self::expectExceptionCode($expectedExceptionCode);
+
+        $this->command->run(new ArrayInput([]), new BufferedOutput());
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function executeThrowsExceptionDataProvider(): array
+    {
+        return [
+            'invalid api token' => [
+                'responseData' => [
+                    HttpResponseFactory::KEY_STATUS_CODE => 401,
+                ],
+                'expectedExceptionClass' => RuntimeException::class,
+                'expectedExceptionMessage' => 'Unauthorized',
+                'expectedExceptionCode' => 401,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider executeDataProvider
+     *
+     * @param array<mixed> $input
+     * @param array<mixed> $httpResponseDataCollection
+     */
+    public function testExecuteSuccess(
+        array $input,
+        array $httpResponseDataCollection,
+        int $expectedReturnCode,
+        string $expectedOutput
+    ): void {
+        foreach ($httpResponseDataCollection as $httpResponseData) {
+            $this->setHttpResponse(
+                $this->httpResponseFactory->createFromArray($httpResponseData)
+            );
+        }
+
+        $output = new BufferedOutput();
+
+        $commandReturnCode = $this->command->run(new ArrayInput($input), $output);
+
+        self::assertSame($expectedReturnCode, $commandReturnCode);
+        self::assertJsonStringEqualsJsonString($expectedOutput, $output->fetch());
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function executeDataProvider(): array
+    {
+        return [
+            'already exists' => [
+                'input' => [],
+                'httpResponseDataCollection' => [
+                    [
+                        HttpResponseFactory::KEY_STATUS_CODE => 200,
+                        HttpResponseFactory::KEY_HEADERS => [
+                            'content-type' => 'application/json; charset=utf-8',
+                        ],
+                        HttpResponseFactory::KEY_BODY => (string) json_encode([
+                            'droplets' => [
+                                [
+                                    'id' => 123,
+                                ]
+                            ],
+                        ]),
+                    ],
+                ],
+                'expectedReturnCode' => Command::SUCCESS,
+                'expectedOutput' => (string) json_encode([
+                    'status' => 'success',
+                    'id' => 123,
+                ]),
+            ],
+            'created' => [
+                'input' => [],
+                'httpResponseDataCollection' => [
+                    [
+                        HttpResponseFactory::KEY_STATUS_CODE => 200,
+                        HttpResponseFactory::KEY_HEADERS => [
+                            'content-type' => 'application/json; charset=utf-8',
+                        ],
+                        HttpResponseFactory::KEY_BODY => (string) json_encode([
+                            'droplets' => [],
+                        ]),
+                    ],
+                    [
+                        HttpResponseFactory::KEY_STATUS_CODE => 200,
+                        HttpResponseFactory::KEY_HEADERS => [
+                            'content-type' => 'application/json; charset=utf-8',
+                        ],
+                        HttpResponseFactory::KEY_BODY => (string) json_encode([
+                            'droplet' => [
+                                'id' => 789,
+                            ],
+                        ]),
+                    ],
+                ],
+                'expectedReturnCode' => Command::SUCCESS,
+                'expectedOutput' => (string) json_encode([
+                    'status' => 'success',
+                    'id' => 789,
+                ]),
+            ],
+        ];
+    }
+
+    private function setHttpResponse(ResponseInterface $response): void
+    {
+        $container = self::getContainer();
+        $mockHandler = $container->get(MockHandler::class);
+        if ($mockHandler instanceof MockHandler) {
+            $mockHandler->append($response);
+        }
+    }
+}
