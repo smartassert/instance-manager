b3e820771a9504d392da292683c58ee0006c4376
Jon Cram <webignition@gmail.com>
2021-10-11 10:06:26 +0100

    Create empty symfony CLI project (#5)

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,28 @@
+<?php
+
+$exclusions = [
+    __DIR__ . '/src/Kernel.php',
+    __DIR__ . '/tests/bootstrap.php',
+];
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+    ->filter(function (\Symfony\Component\Finder\SplFileInfo $file) use ($exclusions) {
+        return !in_array($file->getPathname(), $exclusions);
+    })
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        '@PhpCsFixer' => true,
+        'concat_space' => [
+            'spacing' => 'one',
+        ],
+        'trailing_comma_in_multiline' => false,
+        'php_unit_internal_class' => false,
+        'php_unit_test_class_requires_covers' => false,
+    ])
+    ->setFinder($finder)
+    ;
