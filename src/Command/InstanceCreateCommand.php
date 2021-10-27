02775f353672fc405247c9c1480ee6c16eee83d0
Jon Cram <webignition@gmail.com>
2021-10-27 14:58:59 +0100

    Pass service token direct to app:instance:create

--- src/Command/InstanceCreateCommand.php
+++ src/Command/InstanceCreateCommand.php
@@ -8,6 +8,7 @@ use DigitalOceanV2\Exception\ExceptionInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
@@ -17,6 +18,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 class InstanceCreateCommand extends Command
 {
     public const NAME = 'app:instance:create';
+    public const OPTION_SERVICE_TOKEN = 'service-token';
 
     public function __construct(
         private InstanceRepository $instanceRepository,
@@ -25,14 +27,35 @@ class InstanceCreateCommand extends Command
         parent::__construct(null);
     }
 
+    protected function configure(): void
+    {
+        $this
+            ->addOption(
+                self::OPTION_SERVICE_TOKEN,
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Service token to allow secure post-create calls to instance'
+            )
+        ;
+    }
+
     /**
      * @throws ExceptionInterface
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $serviceToken = $input->getOption(self::OPTION_SERVICE_TOKEN);
+        $serviceToken = (is_string($serviceToken)) ? trim($serviceToken) : '';
+
+        if ('' === $serviceToken) {
+            $output->write($this->outputFactory->createErrorOutput('service-token-missing'));
+
+            return Command::FAILURE;
+        }
+
         $instance = $this->instanceRepository->findCurrent();
         if (null === $instance) {
-            $instance = $this->instanceRepository->create();
+            $instance = $this->instanceRepository->create($serviceToken);
         }
 
         $output->write($this->outputFactory->createSuccessOutput(['id' => $instance->getId()]));
