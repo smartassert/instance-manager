540cd0deb1e7633f24b60036215cbb51e759f5ee
Jon Cram <webignition@gmail.com>
2021-11-03 14:10:19 +0000

    Supply tags for new instance in InstanceConfigurationFactory (#144)

--- src/Services/InstanceConfigurationFactory.php
+++ src/Services/InstanceConfigurationFactory.php
@@ -3,20 +3,30 @@
 namespace App\Services;
 
 use SmartAssert\DigitalOceanDropletConfiguration\Configuration as DropletConfiguration;
-use SmartAssert\DigitalOceanDropletConfiguration\Factory;
+use SmartAssert\DigitalOceanDropletConfiguration\Factory as DropletConfigurationFactory;
 
 class InstanceConfigurationFactory
 {
     public function __construct(
-        private Factory $dropletConfigurationFactory,
+        private DropletConfigurationFactory $dropletConfigurationFactory,
+        private string $instanceCollectionTag,
+        private string $instanceTag,
     ) {
+        $this->instanceCollectionTag = trim($this->instanceCollectionTag);
+        $this->instanceTag = trim($this->instanceTag);
     }
 
     public function create(string $postCreateScript): DropletConfiguration
     {
         $postCreateScript = '' !== $postCreateScript ? $postCreateScript : '# No post-create script';
 
-        $configuration = $this->dropletConfigurationFactory->create();
+        $configuration = $this->dropletConfigurationFactory->create([
+            DropletConfigurationFactory::KEY_TAGS => [
+                $this->instanceCollectionTag,
+                $this->instanceTag,
+            ],
+        ]);
+
         if ('' !== $configuration->getUserData()) {
             $configuration = $configuration->appendUserData("\n\n");
         }
