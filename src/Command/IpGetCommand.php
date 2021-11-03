fbfdbc269216c8d81a69d45d468803b2e3bf36ee
Jon Cram <webignition@gmail.com>
2021-11-03 17:59:47 +0000

    Factor out collection tag from IpGetCommand

--- src/Command/IpGetCommand.php
+++ src/Command/IpGetCommand.php
@@ -3,11 +3,12 @@
 namespace App\Command;
 
 use App\Model\AssignedIp;
+use App\Services\CommandConfigurator;
+use App\Services\CommandInputReader;
 use App\Services\FloatingIpRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
@@ -17,32 +18,26 @@ use Symfony\Component\Console\Output\OutputInterface;
 class IpGetCommand extends Command
 {
     public const NAME = 'app:ip:get';
-    public const OPTION_COLLECTION_TAG = 'collection-tag';
     public const EXIT_CODE_EMPTY_COLLECTION_TAG = 3;
 
     public function __construct(
         private FloatingIpRepository $floatingIpRepository,
+        private CommandConfigurator $configurator,
+        private CommandInputReader $inputReader,
     ) {
         parent::__construct(null);
     }
 
     protected function configure(): void
     {
-        $this
-            ->addOption(
-                self::OPTION_COLLECTION_TAG,
-                null,
-                InputOption::VALUE_REQUIRED,
-                'Tag applied to all instances'
-            )
-        ;
+        $this->configurator->addCollectionTagOption($this);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $collectionTag = $this->getStringOption(self::OPTION_COLLECTION_TAG, $input);
+        $collectionTag = $this->inputReader->getTrimmedStringOption(CommandConfigurator::OPTION_COLLECTION_TAG, $input);
         if ('' === $collectionTag) {
-            $output->writeln('"' . self::OPTION_COLLECTION_TAG . '" option empty');
+            $output->writeln('"' . CommandConfigurator::OPTION_COLLECTION_TAG . '" option empty');
 
             return self::EXIT_CODE_EMPTY_COLLECTION_TAG;
         }
@@ -53,15 +48,5 @@ class IpGetCommand extends Command
         }
 
         return Command::SUCCESS;
-    }
-
-    private function getStringOption(string $name, InputInterface $input): string
-    {
-        $value = $input->getOption($name);
-        if (!is_string($value)) {
-            $value = '';
-        }
-
-        return trim($value);
     }
 }
