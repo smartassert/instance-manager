414020bc804d9d15249e2ec9f39a8f1c4e14c416
Jon Cram <webignition@gmail.com>
2021-11-22 15:11:17 +0000

    Create ServiceConfiguration service (#192)

---
+++ src/Services/ServiceConfiguration.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Model\EnvironmentVariable;
+use App\Model\EnvironmentVariableList;
+
+class ServiceConfiguration
+{
+    public const ENV_VAR_FILENAME = 'env.json';
+
+    public function __construct(
+        private string $configurationDirectory,
+    ) {
+    }
+
+    public function getEnvironmentVariables(string $serviceId): EnvironmentVariableList
+    {
+        $filePath = $this->configurationDirectory . '/' . $serviceId . '/env.json';
+        if (!file_exists($filePath) || !is_readable($filePath)) {
+            return new EnvironmentVariableList([]);
+        }
+
+        $content = (string) file_get_contents($filePath);
+        $data = json_decode($content, true);
+
+        if (false === is_array($data)) {
+            return new EnvironmentVariableList([]);
+        }
+
+        $environmentVariables = [];
+        foreach ($data as $key => $value) {
+            if (is_string($key) && is_string($value)) {
+                $environmentVariables[] = new EnvironmentVariable($key, $value);
+            }
+        }
+
+        return new EnvironmentVariableList($environmentVariables);
+    }
+}
---
+++ tests/Unit/Services/ServiceConfigurationTest.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Tests\Unit\Services;
+
+use App\Model\EnvironmentVariable;
+use App\Model\EnvironmentVariableList;
+use App\Services\ServiceConfiguration;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use phpmock\mockery\PHPMockery;
+use PHPUnit\Framework\TestCase;
+
+class ServiceConfigurationTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    private const SERVICE_CONFIGURATION_DIRECTORY = './services';
+
+    private ServiceConfiguration $serviceConfiguration;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->serviceConfiguration = new ServiceConfiguration(self::SERVICE_CONFIGURATION_DIRECTORY);
+    }
+
+    public function testGetEnvironmentVariablesFileDoesNotExist(): void
+    {
+        $serviceId = 'service_id';
+
+        $expectedFilePath = $this->createExpectedEnvVarFilePath($serviceId);
+
+        $mockNamespace = 'App\Services';
+
+        PHPMockery::mock($mockNamespace, 'file_exists')
+            ->with($expectedFilePath)
+            ->andReturn(false)
+        ;
+
+        $environmentVariableList = $this->serviceConfiguration->getEnvironmentVariables($serviceId);
+
+        self::assertEquals(new EnvironmentVariableList([]), $environmentVariableList);
+    }
+
+    public function testGetEnvironmentVariablesFileIsNotReadable(): void
+    {
+        $serviceId = 'service_id';
+
+        $expectedFilePath = $this->createExpectedEnvVarFilePath($serviceId);
+
+        $mockNamespace = 'App\Services';
+
+        PHPMockery::mock($mockNamespace, 'file_exists')
+            ->with($expectedFilePath)
+            ->andReturn(true)
+        ;
+
+        PHPMockery::mock($mockNamespace, 'is_readable')
+            ->with($expectedFilePath)
+            ->andReturn(false)
+        ;
+
+        $environmentVariableList = $this->serviceConfiguration->getEnvironmentVariables($serviceId);
+
+        self::assertEquals(new EnvironmentVariableList([]), $environmentVariableList);
+    }
+
+    /**
+     * @dataProvider getEnvironmentVariablesSuccessDataProvider
+     */
+    public function testGetEnvironmentVariablesSuccess(
+        string $serviceId,
+        string $envVarFileContent,
+        EnvironmentVariableList $expectedEnvironmentVariables
+    ): void {
+        $expectedFilePath = $this->createExpectedEnvVarFilePath($serviceId);
+
+        $mockNamespace = 'App\Services';
+
+        PHPMockery::mock($mockNamespace, 'file_exists')
+            ->with($expectedFilePath)
+            ->andReturn(true)
+        ;
+
+        PHPMockery::mock($mockNamespace, 'is_readable')
+            ->with($expectedFilePath)
+            ->andReturn(true)
+        ;
+
+        PHPMockery::mock($mockNamespace, 'file_get_contents')
+            ->with($expectedFilePath)
+            ->andReturn($envVarFileContent)
+        ;
+
+        $environmentVariableList = $this->serviceConfiguration->getEnvironmentVariables($serviceId);
+
+        self::assertEquals($expectedEnvironmentVariables, $environmentVariableList);
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function getEnvironmentVariablesSuccessDataProvider(): array
+    {
+        return [
+            'empty' => [
+                'serviceId' => 'service1',
+                'envVarFileContent' => '{}',
+                'expectedEnvironmentVariables' => new EnvironmentVariableList([]),
+            ],
+            'content not a json array' => [
+                'serviceId' => 'service1',
+                'envVarFileContent' => 'true',
+                'expectedEnvironmentVariables' => new EnvironmentVariableList([]),
+            ],
+            'single invalid item, key not a string' => [
+                'serviceId' => 'service2',
+                'envVarFileContent' => '{0:"value1"}',
+                'expectedEnvironmentVariables' => new EnvironmentVariableList([]),
+            ],
+            'single invalid item, value not a string' => [
+                'serviceId' => 'service2',
+                'envVarFileContent' => '{"key1":true}',
+                'expectedEnvironmentVariables' => new EnvironmentVariableList([]),
+            ],
+            'single' => [
+                'serviceId' => 'service2',
+                'envVarFileContent' => '{"key1":"value1"}',
+                'expectedEnvironmentVariables' => new EnvironmentVariableList([
+                    new EnvironmentVariable('key1', 'value1'),
+                ]),
+            ],
+            'multiple' => [
+                'serviceId' => 'service3',
+                'envVarFileContent' => '{"key1":"value1", "key2":"value2"}',
+                'expectedEnvironmentVariables' => new EnvironmentVariableList([
+                    new EnvironmentVariable('key1', 'value1'),
+                    new EnvironmentVariable('key2', 'value2'),
+                ]),
+            ],
+        ];
+    }
+
+    private function createExpectedEnvVarFilePath(string $serviceId): string
+    {
+        return sprintf(
+            '%s/%s/%s',
+            self::SERVICE_CONFIGURATION_DIRECTORY,
+            $serviceId,
+            ServiceConfiguration::ENV_VAR_FILENAME
+        );
+    }
+}
