48931ef115634f5e3aa9cd9062148ca1f0d8786e
Jon Cram <webignition@gmail.com>
2021-11-24 11:31:21 +0000

    Remove command configuration from RetryableCommandTrait

--- src/Command/RetryableCommandTrait.php
+++ src/Command/RetryableCommandTrait.php
@@ -3,7 +3,6 @@
 namespace App\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 
 trait RetryableCommandTrait
 {
@@ -19,35 +18,5 @@ trait RetryableCommandTrait
         $delay = $input->getOption(self::OPTION_RETRY_DELAY);
 
         return is_numeric($delay) ? (int) $delay : self::DEFAULT_RETRY_DELAY;
-    }
-
-    abstract protected function getDefaultRetryLimit(): int;
-
-    abstract protected function getDefaultRetryDelay(): int;
-
-    abstract protected function getRetryLimitOptionName(): string;
-
-    abstract protected function getRetryDelayOptionName(): string;
-
-    protected function configure(): void
-    {
-        parent::configure();
-
-        $this
-            ->addOption(
-                $this->getRetryLimitOptionName(),
-                null,
-                InputOption::VALUE_REQUIRED,
-                'How many times to retry command is not successful',
-                $this->getDefaultRetryLimit()
-            )
-            ->addOption(
-                $this->getRetryDelayOptionName(),
-                null,
-                InputOption::VALUE_REQUIRED,
-                'How long to wait, in seconds, if command is not successful',
-                $this->getDefaultRetryDelay()
-            )
-        ;
     }
 }
