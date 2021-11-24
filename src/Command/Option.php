472fd38949d34f79111dce231befd9c6d77fc06c
Jon Cram <webignition@gmail.com>
2021-11-24 14:43:55 +0000

    Create Command\Option

---
+++ src/Command/Option.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Command;
+
+class Option
+{
+    public const OPTION_COLLECTION_TAG = 'collection-tag';
+    public const OPTION_IMAGE_ID = 'image-id';
+    public const OPTION_RETRY_LIMIT = 'retry-limit';
+    public const OPTION_RETRY_DELAY = 'retry-delay';
+    public const OPTION_ID = 'id';
+}
