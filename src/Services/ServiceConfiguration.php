d5c2b84af89cfb2e3f666b8e236f63bcf9bba33b
Jon Cram <webignition@gmail.com>
2021-11-24 11:13:50 +0000

    Add ServiceConfiguration::getHealthCheckUrl() (#213)

--- src/Services/ServiceConfiguration.php
+++ src/Services/ServiceConfiguration.php
@@ -7,6 +7,7 @@ use App\Model\EnvironmentVariableList;
 class ServiceConfiguration
 {
     public const ENV_VAR_FILENAME = 'env.json';
+    public const CONFIGURATION_FILENAME = 'configuration.json';
 
     public function __construct(
         private string $configurationDirectory,
@@ -15,17 +16,7 @@ class ServiceConfiguration
 
     public function getEnvironmentVariables(string $serviceId): EnvironmentVariableList
     {
-        $filePath = $this->configurationDirectory . '/' . $serviceId . '/env.json';
-        if (!file_exists($filePath) || !is_readable($filePath)) {
-            return new EnvironmentVariableList([]);
-        }
-
-        $content = (string) file_get_contents($filePath);
-        $data = json_decode($content, true);
-
-        if (false === is_array($data)) {
-            return new EnvironmentVariableList([]);
-        }
+        $data = $this->readJsonFileToArray($serviceId, self::ENV_VAR_FILENAME);
 
         $environmentVariables = [];
         foreach ($data as $key => $value) {
@@ -35,5 +26,33 @@ class ServiceConfiguration
         }
 
         return new EnvironmentVariableList($environmentVariables);
+    }
+
+    public function getHealthCheckUrl(string $serviceId): ?string
+    {
+        $data = $this->readJsonFileToArray($serviceId, self::CONFIGURATION_FILENAME);
+        $healthCheckUrl = $data['health_check_url'] ?? null;
+
+        return is_string($healthCheckUrl) ? $healthCheckUrl : null;
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    private function readJsonFileToArray(string $serviceId, string $filename): array
+    {
+        $filePath = $this->configurationDirectory . '/' . $serviceId . '/' . $filename;
+        if (!file_exists($filePath) || !is_readable($filePath)) {
+            return [];
+        }
+
+        $content = (string) file_get_contents($filePath);
+        $data = json_decode($content, true);
+
+        if (false === is_array($data)) {
+            return [];
+        }
+
+        return $data;
     }
 }
