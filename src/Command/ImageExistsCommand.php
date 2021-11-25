13780ae559ef01560ac593bc603afe86e34149d5
Jon Cram <webignition@gmail.com>
2021-11-25 15:45:47 +0000

    Create app:image:exists

---
+++ src/Command/ImageExistsCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Command;
+
+use App\Services\CommandConfigurator;
+use App\Services\CommandInputReader;
+use App\Services\ImageRepository;
+use App\Services\ServiceConfiguration;
+use DigitalOceanV2\Exception\ExceptionInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: ImageExistsCommand::NAME,
+    description: 'Check if an image exists',
+)]
+class ImageExistsCommand extends Command
+{
+    public const NAME = 'app:image:exists';
+
+    public const EXIT_CODE_EMPTY_SERVICE_ID = 3;
+    public const EXIT_CODE_MISSING_IMAGE_ID = 4;
+
+    public function __construct(
+        private CommandConfigurator $configurator,
+        private CommandInputReader $inputReader,
+        private ServiceConfiguration $serviceConfiguration,
+        private ImageRepository $imageRepository,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->configurator->addServiceIdOption($this);
+    }
+
+    /**
+     * @throws ExceptionInterface
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $serviceId = $this->inputReader->getTrimmedStringOption(Option::OPTION_SERVICE_ID, $input);
+        if ('' === $serviceId) {
+            $output->writeln('"' . Option::OPTION_SERVICE_ID . '" option empty');
+
+            return self::EXIT_CODE_EMPTY_SERVICE_ID;
+        }
+
+        $imageId = $this->serviceConfiguration->getImageId($serviceId);
+        if (null === $imageId) {
+            $output->writeln('image_id missing');
+
+            return self::EXIT_CODE_MISSING_IMAGE_ID;
+        }
+
+        return $this->imageRepository->exists($imageId) ? Command::SUCCESS : Command::FAILURE;
+    }
+}
---
+++ tests/Functional/Command/ImageExistsCommandTest.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Tests\Functional\Command;
+
+use App\Command\ImageExistsCommand;
+use App\Command\Option;
+use App\Services\ServiceConfiguration;
+use App\Tests\Services\HttpResponseFactory;
+use DigitalOceanV2\Exception\RuntimeException;
+use GuzzleHttp\Handler\MockHandler;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\NullOutput;
+use webignition\ObjectReflector\ObjectReflector;
+
+class ImageExistsCommandTest extends KernelTestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    private const SERVICE_ID = 'service_id';
+    private const IMAGE_ID = '12345';
+
+    private ImageExistsCommand $command;
+    private HttpResponseFactory $httpResponseFactory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $command = self::getContainer()->get(ImageExistsCommand::class);
+        \assert($command instanceof ImageExistsCommand);
+        $this->command = $command;
+
+        $httpResponseFactory = self::getContainer()->get(HttpResponseFactory::class);
+        \assert($httpResponseFactory instanceof HttpResponseFactory);
+        $this->httpResponseFactory = $httpResponseFactory;
+    }
+
+    /**
+     * @dataProvider runThrowsExceptionDataProvider
+     *
+     * @param array<mixed>             $responseData
+     * @param class-string<\Throwable> $expectedExceptionClass
+     */
+    public function testRunThrowsException(
+        array $responseData,
+        string $expectedExceptionClass,
+        string $expectedExceptionMessage,
+        int $expectedExceptionCode
+    ): void {
+        $httpResponse = $this->httpResponseFactory->createFromArray($responseData);
+        $this->setHttpResponse($httpResponse);
+
+        self::expectException($expectedExceptionClass);
+        self::expectExceptionMessage($expectedExceptionMessage);
+        self::expectExceptionCode($expectedExceptionCode);
+
+        $this->mockServiceConfigurationGetImageId(self::IMAGE_ID);
+
+        $this->command->run(
+            new ArrayInput([
+                '--' . Option::OPTION_SERVICE_ID => 'service_id',
+            ]),
+            new BufferedOutput()
+        );
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function runThrowsExceptionDataProvider(): array
+    {
+        return [
+            'invalid api token' => [
+                'responseData' => [
+                    HttpResponseFactory::KEY_STATUS_CODE => 401,
+                ],
+                'expectedExceptionClass' => RuntimeException::class,
+                'expectedExceptionMessage' => 'Unauthorized',
+                'expectedExceptionCode' => 401,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider runSuccessDataProvider
+     *
+     * @param array<mixed>             $input
+     * @param array<int, array<mixed>> $httpResponseDataCollection
+     */
+    public function testRunSuccess(
+        array $input,
+        array $httpResponseDataCollection,
+        int $expectedReturnCode
+    ): void {
+        foreach ($httpResponseDataCollection as $httpResponseData) {
+            $this->setHttpResponse(
+                $this->httpResponseFactory->createFromArray($httpResponseData)
+            );
+        }
+
+        $this->mockServiceConfigurationGetImageId(self::IMAGE_ID);
+
+        $commandReturnCode = $this->command->run(new ArrayInput($input), new NullOutput());
+
+        self::assertSame($expectedReturnCode, $commandReturnCode);
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function runSuccessDataProvider(): array
+    {
+        return [
+            'not exists' => [
+                'input' => [
+                    '--' . Option::OPTION_SERVICE_ID => 'service_id',
+                ],
+                'httpResponseDataCollection' => [
+                    [
+                        HttpResponseFactory::KEY_STATUS_CODE => 404,
+                    ],
+                ],
+                'expectedReturnCode' => Command::FAILURE,
+            ],
+            'exists' => [
+                'input' => [
+                    '--' . Option::OPTION_SERVICE_ID => 'service_id',
+                ],
+                'httpResponseDataCollection' => [
+                    [
+                        HttpResponseFactory::KEY_STATUS_CODE => 200,
+                        HttpResponseFactory::KEY_HEADERS => [
+                            'content-type' => 'application/json; charset=utf-8',
+                        ],
+                        HttpResponseFactory::KEY_BODY => (string) json_encode([
+                            'image' => [
+                                'id' => 123,
+                            ],
+                        ]),
+                    ],
+                ],
+                'expectedReturnCode' => Command::SUCCESS,
+            ],
+        ];
+    }
+
+    private function setHttpResponse(ResponseInterface $response): void
+    {
+        $container = self::getContainer();
+        $mockHandler = $container->get(MockHandler::class);
+        if ($mockHandler instanceof MockHandler) {
+            $mockHandler->append($response);
+        }
+    }
+
+    private function mockServiceConfigurationGetImageId(?string $imageId): void
+    {
+        $serviceConfiguration = \Mockery::mock(ServiceConfiguration::class);
+        $serviceConfiguration
+            ->shouldReceive('getImageId')
+            ->with(self::SERVICE_ID)
+            ->andReturn($imageId)
+        ;
+
+        ObjectReflector::setProperty(
+            $this->command,
+            $this->command::class,
+            'serviceConfiguration',
+            $serviceConfiguration
+        );
+    }
+}
