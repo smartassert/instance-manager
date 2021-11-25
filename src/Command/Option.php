38b7044694dd5b1427043e29888134c7c395aa3e
Jon Cram <webignition@gmail.com>
2021-11-25 12:18:44 +0000

    Remove CommandConfigurator::addImageId() (#254)

--- src/Command/Option.php
+++ src/Command/Option.php
@@ -5,7 +5,6 @@ namespace App\Command;
 class Option
 {
     public const OPTION_SERVICE_ID = 'service-id';
-    public const OPTION_IMAGE_ID = 'image-id';
     public const OPTION_RETRY_LIMIT = 'retry-limit';
     public const OPTION_RETRY_DELAY = 'retry-delay';
     public const OPTION_ID = 'id';
