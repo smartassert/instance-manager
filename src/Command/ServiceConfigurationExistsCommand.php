79efd53259b89c9cf69a458fc248281a1db14907
Jon Cram <webignition@gmail.com>
2021-11-25 10:59:21 +0000

    Fix command name

--- src/Command/ServiceConfigurationExistsCommand.php
+++ src/Command/ServiceConfigurationExistsCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
-    name: InstanceCreateCommand::NAME,
+    name: ServiceConfigurationExistsCommand::NAME,
     description: 'Check if configuration exists for a given service_id',
 )]
 class ServiceConfigurationExistsCommand extends Command
