e1226980c800a5f6bcef0d4f3abb25326dc3ac5d
Jon Cram <webignition@gmail.com>
2021-11-26 16:37:02 +0000

    Re-implement InstanceCollection as doctrine collection (#267)

--- src/Model/InstanceCollection.php
+++ src/Model/InstanceCollection.php
@@ -4,54 +4,24 @@ namespace App\Model;
 
 use App\Model\InstanceSorter\InstanceCreatedDateSorter;
 use App\Model\InstanceSorter\InstanceSorterInterface;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
- * @implements \IteratorAggregate<int|string, Instance>
+ * @extends ArrayCollection<int|string, Instance>
  */
-class InstanceCollection implements \IteratorAggregate, \Countable, \JsonSerializable
+class InstanceCollection extends ArrayCollection implements \JsonSerializable
 {
-    /**
-     * @var Instance[]
-     */
-    private array $instances;
-
-    /**
-     * @param Instance[] $instances
-     */
-    public function __construct(array $instances)
-    {
-        $this->instances = array_filter($instances, function ($item) {
-            return $item instanceof Instance;
-        });
-    }
-
-    public function getIterator(): \Traversable
-    {
-        return new \ArrayIterator($this->instances);
-    }
-
-    public function count(): int
-    {
-        return count($this->instances);
-    }
-
-    public function getFirst(): ?Instance
-    {
-        return 0 === count($this)
-            ? null
-            : $this->instances[0];
-    }
-
     public function getNewest(): ?Instance
     {
         $sortedCollection = $this->sortByCreatedDate();
+        $first = $sortedCollection->first();
 
-        return $sortedCollection->getFirst();
+        return $first instanceof Instance ? $first : null;
     }
 
     public function sort(InstanceSorterInterface $sorter): self
     {
-        $instances = $this->instances;
+        $instances = $this->toArray();
 
         usort($instances, function (Instance $a, Instance $b) use ($sorter): int {
             return $sorter->sort($a, $b);
@@ -60,17 +30,13 @@ class InstanceCollection implements \IteratorAggregate, \Countable, \JsonSeriali
         return new InstanceCollection($instances);
     }
 
-    public function filter(Filter $filter): self
+    public function filterByFilter(Filter $filter): self
     {
-        $instances = [];
+        $filteredCollection = $this->filter(function (Instance $instance) use ($filter) {
+            return $instance->isMatchedBy($filter);
+        });
 
-        foreach ($this as $instance) {
-            if ($instance->isMatchedBy($filter)) {
-                $instances[] = $instance;
-            }
-        }
-
-        return new InstanceCollection($instances);
+        return new InstanceCollection(array_values($filteredCollection->toArray()));
     }
 
     /**
