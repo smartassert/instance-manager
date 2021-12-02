c1eb572c1cbcb59979c70dfde7f66dc94df2cbd1
Jon Cram <webignition@gmail.com>
2021-12-02 11:36:39 +0000

    Verify multi-line env var values serialize to strings

--- tests/Unit/Model/EnvironmentVariableTest.php
+++ tests/Unit/Model/EnvironmentVariableTest.php
@@ -9,21 +9,56 @@ use PHPUnit\Framework\TestCase;
 
 class EnvironmentVariableTest extends TestCase
 {
-    public function testToString(): void
+    /**
+     * @dataProvider toStringDataProvider
+     */
+    public function testToString(EnvironmentVariable $environmentVariable, string $expected): void
     {
-        self::assertSame(
-            '',
-            (string) new EnvironmentVariable('', '')
-        );
+        self::assertSame($expected, (string) $environmentVariable);
+    }
 
-        self::assertSame(
-            'key=""',
-            (string) new EnvironmentVariable('key', '')
-        );
+    /**
+     * @return array<mixed>
+     */
+    public function toStringDataProvider(): array
+    {
+        $publicKeyValue = <<<'END'
+            -----BEGIN PUBLIC KEY-----
+            MIIBITANBgkqhkiG9w0BAQEFAAOCAQ4AMIIBCQKCAQBCT64wYsQ8qPbgnhnS+6TJ
+            CuLonpGwbPiPEq11y5SeQGRm/O4nqH8ZQg6O6dvqwrQA3A9KDz0M91Z3KO3/2WbN
+            ndGvF1LU23krIAYUdJP384iWkdz6qGkmPY8nwKrwhN0E4GFPigHAk3l1NkHHVDuk
+            Ad/5WpMSgOQ4gcdvmmC/fFgRb09QdNm/tpgTt59IVWA5xnmeGMrqrZjK9QHQr7/e
+            r9KCwNsThZ0hUAUvt5GJB5J2tYibvgjpfmOiomrVRP1JlUWvJoegds7GomsRwk7f
+            mN0Kep9ZPydi/VV4bjo6xM/Lg0kvyu8ZBhdekMGe+u0esPZef05xWnDVm5DqOhxV
+            AgMBAAE=
+            -----END PUBLIC KEY-----
+            END;
 
-        self::assertSame(
-            'key="value"',
-            (string) new EnvironmentVariable('key', 'value')
-        );
+        return [
+            'empty key, empty value' => [
+                'environmentVariable' => new EnvironmentVariable('', ''),
+                'expected' => '',
+            ],
+            'empty key, has value' => [
+                'environmentVariable' => new EnvironmentVariable('', 'value'),
+                'expected' => '',
+            ],
+            'has key, empty value' => [
+                'environmentVariable' => new EnvironmentVariable('key', ''),
+                'expected' => 'key=""',
+            ],
+            'has key, has single-line value' => [
+                'environmentVariable' => new EnvironmentVariable('key', 'value'),
+                'expected' => 'key="value"',
+            ],
+            'has key, has single-line value containing quotes' => [
+                'environmentVariable' => new EnvironmentVariable('key', 'va"l"ue'),
+                'expected' => 'key="va\"l\"ue"',
+            ],
+            'has key, has multi-line value (2048-bit public key)' => [
+                'environmentVariable' => new EnvironmentVariable('key', $publicKeyValue),
+                'expected' => 'key="' . $publicKeyValue . '"',
+            ],
+        ];
     }
 }
